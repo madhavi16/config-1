1099819a5391fdfe6e21c37a1f3789afb098e39c
Madhavi016 <me@here.com>
2013-11-19 14:02:08 +0300

    Added piston config file to the repository.

---
+++ piston.cmake
@@ -0,0 +1,9 @@
+set(PISTON_PACKAGE_VERSION 0.1)
+set(PISTON_DEPENDS OpenGL MPI  VTK Qt4) 
+set(PISTON_BUILD_DEMO ON)
+set(PISTON_USE_CUDA ON)
+set(PISTON_REPO_TYPE git)
+set(PISTON_REPO_URL https://github.com/losalamos/PISTON)
+set(PISTON_REPO_TAG master)
+set(PISTON_SOURCE "${CMAKE_SOURCE_DIR}/src/Piston")
+
